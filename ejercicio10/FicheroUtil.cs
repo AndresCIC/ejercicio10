e6e63853656a54d35db1370f50b1081ae460fdf0
cursocic01 <cursocic01@outlook.com>
2017-07-10 15:55:56 +0200

    Primer commit

---
+++ ejercicio10/FicheroUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio10
+{
+    public class FicheroUtil
+    {
+        public void AlmacenarDatos(Object datos, string nombreFichero)
+        {
+            //TODO
+            //throw new Exception("No implementado aun");
+        }            
+    }
+}
